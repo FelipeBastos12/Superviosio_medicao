b091575f8390bed4214cde5ddb0207a3d65becc3
FelipeBastos12 <92400735+FelipeBastos12@users.noreply.github.com>
2025-08-03 20:59:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from datetime import datetime, timedelta
 
 # --- CONFIGURAÇÕES ---
 PATHS = {
-    "A": "Planilha_242_LAT - FASEA",
-    "B": "Planilha_242_LAT - FASEB",
-    "C": "Planilha_242_LAT - FASEC"
+    "A": "Planilha_242_LAT - FASEA.csv",
+    "B": "Planilha_242_LAT - FASEB.csv",
+    "C": "Planilha_242_LAT - FASEC.csv"
 }
 REFRESH_INTERVAL_MS = 500
 
@@ -329,8 +329,3 @@ fig.update_layout(
     template="simple_white"
 )
 st.plotly_chart(fig, use_container_width=True)
-
-
-
-
-
