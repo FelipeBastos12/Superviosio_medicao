8bc86fcd44bffebd058b80231a0b3eec678a400c
FelipeBastos12 <92400735+FelipeBastos12@users.noreply.github.com>
2025-08-04 09:16:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ import collections
 # --- CONFIGURAÇÕES ---
 PATHS = {
     "A": "Medicoes_FASEA(1).csv",
-    "B": "Medicoes_FASEB(2).csv",
-    "C": "Medicoes_FASEC(3).csv"
+    "B": "Medicoes_FASEB(1).csv",
+    "C": "Medicoes_FASEC(1).csv"
 }
 REFRESH_INTERVAL_MS = 500
 
@@ -567,6 +567,7 @@ with st.expander("Log de alarmes"):
             st.error(erro)
     else:
         st.info("Nenhum alarme registrado.")
+
 
 
 
